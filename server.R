a66727c82a987a5ce1197cdaa9415349bf22971c
Raquel Bonilla <raquelb.2014@gmail.com>
2018-11-15 14:05:52 -0800

    proj so far

--- server.R
+++ server.R
@@ -1,9 +1,36 @@
 library(tidyverse)
+library(rlang)
+
+
+wine <- read.csv("../CourseDataSets/wine-reviews/winemag-data-130k-v2.csv")
+
+USwine <- subset(wine, wine$country == "US")
+stateavg <- USwine %>%
+  group_by(province) %>%
+  summarize(avgPoints = mean(points))
+
+countries <- wine %>%
+              select(country)
+countries <- unique(countries)
+countries <- sort(countries$country)
+
 function(input, output, session) {
+  
+  reactive({
+   USwine <- subset(wine, wine$country == input$selectCountry)
+    
+    stateavg <- USwine %>%
+      group_by(province) %>%
+      summarize(avgPoints = mean(points))
+  })
+  
+  
+  
   output$plot1 <- renderPlot({
-    iris %>%
-      ggplot(aes(x = Sepal.Length, y = Petal.Length)) +
-      geom_point()
+    ggplot(stateavg, aes(x=province, y = avgPoints, fill = province)) + 
+      geom_bar(stat = "identity") + coord_cartesian(ylim = c(min(stateavg$avgPoints) - 2, max(stateavg$avgPoints) + 2)) + 
+      theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0), legend.position="none") + 
+      labs(title = "Average Points of All Wines reviewed by US State", x = "State", y ="Average Points Earned")
   })
   output$plot2 <- renderPlot({
     iris %>%
--- ui.R
+++ ui.R
@@ -2,6 +2,12 @@ pageWithSidebar(
   headerPanel('Iris Data'),
   sidebarPanel(
     ## Input Widgets
+    selectInput("selectCountry", label = h3("Country"), 
+                choices = as.list(countries), 
+                selected = "US"),
+    
+    hr(),
+    fluidRow(column(3, verbatimTextOutput("value")))
   ),
   mainPanel(
     plotOutput('plot1'),
