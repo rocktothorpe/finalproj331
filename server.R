9c303f547e0d72ef9c78661f1875df99f1d1c4db
Connor Graves <citygraves18@gmail.com>
2018-11-15 22:46:29 -0800

    Worked on it
    Moved country and reviewer name lists to UI. (They only worked because you had them in your workspace, and only UI needs them, not Server)
    
    Added tab navigation
    
    Added support to subset by wine variety as well as country

--- server.R
+++ server.R
@@ -8,7 +8,7 @@ library(ggplot2)
 # setwd("../finalproj331/")
 wine <- read.csv("winemag-data-130k-v2.csv")
 wine <- wine %>%
-          slice(1:40000)
+  slice(1:40000)
 
 # For wine points / country graph
 USwine <- subset(wine, wine$country == "US")
@@ -17,13 +17,13 @@ stateavg <- USwine %>%
   summarize(avgPoints = mean(points))
 
 countries <- wine %>%
-              select(country)
+  select(country)
 countries <- unique(countries)
 countries <- sort(countries$country)
 
 # For review ratings on wine graph
 reviewerList <- wine %>%
-                  select(taster_name)
+  select(taster_name)
 reviewerList <- unique(reviewerList$taster_name)
 reviewers  <- wine %>%
   filter(taster_name == "Alexander Peartree")  %>%# the user can switch the name; maybe a toggle dropdown
@@ -33,28 +33,31 @@ reviewers  <- wine %>%
 
 reviewers <- unique(reviewers)
 reviewers<- reviewers %>%
-              arrange(taster_name)
+  arrange(taster_name)
 num_to_display <- 5
 
 function(input, output, session) {
   
-  output$plot1 <- renderPlot({
-   USwine <- subset(wine, wine$country == input$selectCountry)
-
+  output$countryPlot <- renderPlot({
+    if(input$selectVariety == "All")
+      USwine <- subset(wine, wine$country == input$selectCountry)
+    if(input$selectVariety != "All")
+      USwine <- subset(wine, wine$country == input$selectCountry & wine$variety == input$selectVariety)
+    
     stateavg <- USwine %>%
       group_by(province) %>%
       summarize(avgPoints = mean(points))
-
+    
     ggplot(stateavg, aes(x=province, y = avgPoints, fill = province)) +
       geom_bar(stat = "identity") + coord_cartesian(ylim = c(min(stateavg$avgPoints) - 2, max(stateavg$avgPoints) + 2)) +
       theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0), legend.position="none") +
-      labs(title = "Average Points of All Wines reviewed by Region of a Country", x = "Region", y ="Average Points Earned")
+      labs(title = paste("Average Points of", input$selectVariety, "Wines reviewed by Region of", input$selectCountry), x = "Region", y ="Average Points Earned")
     
   })
-  output$plot2 <- renderPlot({
-
+  output$reviewPlot <- renderPlot({
+    
     reviewers  <- wine %>%
-      filter(taster_name == "Alexander Peartree")  %>%# the user can switch the name; maybe a toggle dropdown
+      filter(taster_name == input$selectReviewer)  %>%# the user can switch the name; maybe a toggle dropdown
       arrange(desc(points)) %>%
       select(taster_name, points, title, region_1)
     #
@@ -63,16 +66,11 @@ function(input, output, session) {
     #
     reviewers <- reviewers %>%
       slice(1:num_to_display)
-
-     ggplot(reviewers, aes(x=title, y=points, fill=region_1)) +
-       theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0), legend.position="none") +
-       geom_bar(stat="identity") + coord_cartesian(ylim = c(80, 100)) +
-       labs(title = "Points by Reviewer", x = "Wine", y ="Points")
-     
-
-  })
-  output$plot3 <- renderPlot({
-    ggplot(wine, aes(points,price)) + geom_point() + labs(title = "Points VS Price", x = "Points", y ="Price")
+    
+    ggplot(reviewers, aes(x=title, y=points, fill=region_1)) +
+      theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0), legend.position="none") +
+      geom_bar(stat="identity") + coord_cartesian(ylim = c(80, 100)) +
+      labs(title = "Points by Reviewer", x = "Wine", y ="Points")
     
     
   })
--- ui.R
+++ ui.R
@@ -1,18 +1,51 @@
-pageWithSidebar(
-  headerPanel('Wine Data'),
-  sidebarPanel(
-    ## Input Widgets
-    selectInput("selectCountry", label = h3("Country"),
-                choices = as.list(countries),
-                selected = "US"), hr(), fluidRow(column(3, verbatimTextOutput("value")))
-    # selectInput("selectReviewer", label = h3("Reviewer"),
-    #             choices = as.list(reviewerList),
-    #             selected = "Alexander Peartree"), hr(), fluidRow(column(3, verbatimTextOutput("value")))
-  ),
-  mainPanel(
-      plotOutput('plot1'),
-      plotOutput('plot2'),
-      plotOutput('plot3')
-      
-    )
-)
+library(tidyverse)
+
+wine <- read.csv("winemag-data-130k-v2.csv")
+
+countries <- wine %>%
+  select(country)
+countries <- unique(countries)
+countries <- sort(countries$country)
+
+varieties <- wine %>%
+  select(variety)
+varieties <- unique(varieties)
+varieties <- sort(varieties$variety)
+
+reviewers  <- wine %>%
+  select(taster_name)
+
+reviewers <- unique(reviewers)
+reviewers<- sort(reviewers$taster_name)
+
+panel1 <- tabPanel("Score by Country",
+                   sidebarLayout(
+                     sidebarPanel(
+                       selectInput("selectCountry", label = h3("Country"),
+                                   choices = as.list(countries),
+                                   selected = "US"),
+                       selectInput("selectVariety", label = h3("Variety"),
+                                   choices = c("All", as.list(varieties)),
+                                   selected = "All"),
+                       hr()
+                     ),
+                     mainPanel(
+                       plotOutput('countryPlot')
+                     )
+                   )
+          )
+
+panel2 <- tabPanel("Reviewer Scores",
+                   sidebarLayout(
+                     sidebarPanel(
+                       selectInput("selectReviewer", label = h3("Reviewer"),
+                                   choices = as.list(reviewers),
+                                   selected = "Alexander Peartree"), hr()
+                     ),
+                     mainPanel(
+                       plotOutput('reviewPlot')
+                     )
+                   )
+          )
+
+navbarPage("Wine Stats!", panel1, panel2)
